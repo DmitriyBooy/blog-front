c3d72006411256b33ae04765a08021d03aa3aee6
DmitriyBooy <dimkabumiK@gmail.com>
2023-01-25 00:02:53 +0300

    Поменял скролл

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="bg-black h-screen">
+  <div class="app-body bg-black h-screen">
     <router-view/>
   </div>
 </template>
+
+<style lang="scss">
+*::-webkit-scrollbar {
+  cursor: pointer;
+  width: 6px;               /* width of the entire scrollbar */
+}
+
+*::-webkit-scrollbar-track {
+  background: black;        /* color of the tracking area */
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: rgba(255, 255, 255, 0.05);    /* color of the scroll thumb */
+  border-radius: 20px;       /* roundness of the scroll thumb */
+}
+</style>
