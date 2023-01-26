b2b5b70cb77951970094ce928b0ef101e161399a
DmitriyBooy <dimkabumiK@gmail.com>
2023-01-26 22:28:17 +0300

    Подключил пинию

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
-import store from './store'
 import BButton from './components/BButton.vue'
+import { createPinia } from 'pinia'
 
 import './assets/css/main.scss'
 import './assets/css/tailwind.css'
@@ -10,8 +10,10 @@ import './assets/css/tailwind.css'
 import antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
+const pinia = createPinia()
+
 const app = createApp(App)
-    .use(store)
+    .use(pinia)
     .use(router)
     .use(antd)
     .component('b-button', BButton)
