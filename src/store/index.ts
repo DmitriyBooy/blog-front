33aef6ec4dd89bb59b67d155bb9edb94ed9b8ecb
DmitriyBooy <dimkabumiK@gmail.com>
2023-01-26 22:26:21 +0300

    Заполнил стор пользователя

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,15 @@
-import { createStore } from 'vuex'
+import { defineStore } from 'pinia'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+export const UserStore = defineStore('user', {
+  state: () => ({
+    name: 'White dice',
+    login: '@White_dice',
+    id: 1,
+    avatar: 'https://bipbap.ru/wp-content/uploads/2022/11/1652235714_41-kartinkin-net-p-prikolnie-kartinki-dlya-stima-44.jpg',
+    posts: 4,
+    comments: 12,
+    likes: 33,
+  })
 })
+
+export default UserStore
